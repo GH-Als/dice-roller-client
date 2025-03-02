ea1e4abd58928d4de56b92b4552d628e49d4dc9a
GH-Als <135543276+GH-Als@users.noreply.github.com>
2025-03-02 23:52:37 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("rollDice").addEventListener("click", async () => {
+  const diceResult = document.getElementById("diceResult");
+  diceResult.innerText = "Rolling... 🎲";
+
+  try {
+    let response = await fetch(
+      "https://dice-roller-api1.azurewebsites.net/roll-dice"
+    );
+    let data = await response.json();
+    diceResult.innerText = `🎲 You rolled: ${data.diceRoll}`;
+  } catch (error) {
+    diceResult.innerText = "❌ Error rolling dice!";
+  }
+});
